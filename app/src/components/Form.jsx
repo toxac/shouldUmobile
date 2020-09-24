d58e4dda2d5f962d2889319c57ae9c3692cd4f5f
ac <derocon@gmail.com>
2020-09-24 22:40:23 +0530

    form

--- app/src/components/Form.jsx
+++ app/src/components/Form.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { useFormik } from "formik";
 
 const Form = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      appCategory: "",
+      appUsageFrequency: "",
+      appDiscovery: "",
+    },
+    validate,
+    onSubmit: () => {
+      console.log("hey");
+    },
+    onReset: () => {
+      console.log("hey again");
+    },
+  });
+
   return (
     <div className="something">
       <h1>This is a Form</h1>
