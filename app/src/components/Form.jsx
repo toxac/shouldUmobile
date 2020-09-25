f6759994efe141a062aa6d1e662e5bf39e0dd307
ac <derocon@gmail.com>
2020-09-25 23:26:18 +0530

    form options

--- app/src/components/Form.jsx
+++ app/src/components/Form.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { useFormik } from "formik";
+import formOptions from "../data/formOptions";
+
+const validate = (values) => {
+  const errors = {};
+
+  return errors;
+};
 
 const Form = () => {
   const formik = useFormik({
     initialValues: {
       email: "",
       appCategory: "",
+      appType: "",
       appUsageFrequency: "",
       appDiscovery: "",
     },
@@ -19,8 +27,81 @@ const Form = () => {
   });
 
   return (
-    <div className="something">
-      <h1>This is a Form</h1>
+    <div className="section">
+      <div className="container">
+        <div className="column">
+          <form onSubmit={formik.handleSubmit}>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-primary"
+                  type="text"
+                  name="email"
+                  value={formik.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  placeholder="email"
+                />
+              </div>
+            </div>
+
+            <div className="field">
+              <div className="control">
+                <div className="select is-primary is-fullwidth">
+                  <select
+                    value={formik.appCategory}
+                    name="appCategory"
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  >
+                    <option value="">Select a category of the app</option>
+                    {formOptions.appCategory.map((category) => {
+                      return (
+                        <option value={category.id} key={category.id}>
+                          {category.categoryName}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            <div className="field">
+              <div className="control">
+                <div className="select is-primary is-fullwidth">
+                  <select
+                    value={formik.appType}
+                    name="appType"
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  >
+                    <option value="">Select type of the app</option>
+                    {formOptions.appType.map((category) => {
+                      return (
+                        <option value={category.id} key={category.id}>
+                          {category.type}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div>
+              {formOptions.appFeatures.map((feature) => {
+                return (
+                  <span className="tag is-light is-large mr-2 mb-2">
+                    {feature.feature}
+                  </span>
+                );
+              })}
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- app/src/data/formOptions.js
+++ app/src/data/formOptions.js
@@ -1,137 +1,164 @@
 const appCategory = [{
         "categoryName": "Books",
         "percentApps": 0.052,
-        "percentPopApps": 0.004
+        "percentPopApps": 0.004,
+        id: 1
     },
     {
         "categoryName": "Business",
         "percentApps": 0.058,
-        "percentPopApps": 0.002
+        "percentPopApps": 0.002,
+        id: 2
     },
     {
         "categoryName": "Comics",
         "percentApps": 0.005,
-        "percentPopApps": 0.009
+        "percentPopApps": 0.009,
+        id: 3
     },
     {
         "categoryName": "Community",
         "percentApps": 0.022,
-        "percentPopApps": 0.018
+        "percentPopApps": 0.018,
+        id: 4
     },
     {
         "categoryName": "Education",
         "percentApps": 0.071,
-        "percentPopApps": 0.003
+        "percentPopApps": 0.003,
+        id: 5
     },
     {
         "categoryName": "Entertainment",
         "percentApps": 0.095,
-        "percentPopApps": 0.009
+        "percentPopApps": 0.009,
+        id: 6
     },
     {
         "categoryName": "Finance",
         "percentApps": 0.021,
-        "percentPopApps": 0.007
+        "percentPopApps": 0.007,
+        id: 7
     },
     {
         "categoryName": "Health",
         "percentApps": 0.027,
-        "percentPopApps": 0.006
+        "percentPopApps": 0.006,
+        id: 8
     },
     {
         "categoryName": "Libraries",
         "percentApps": 0.003,
-        "percentPopApps": 0.008
+        "percentPopApps": 0.008,
+        id: 9
     },
     {
         "categoryName": "Lifestyle",
         "percentApps": 0.072,
-        "percentPopApps": 0.005
+        "percentPopApps": 0.005,
+        id: 10
     },
     {
         "categoryName": "Video Player",
         "percentApps": 0.019,
-        "percentPopApps": 0.016
+        "percentPopApps": 0.016,
+        id: 11
     },
     {
         "categoryName": "Medical",
         "percentApps": 0.011,
-        "percentPopApps": 0.002
+        "percentPopApps": 0.002,
+        id: 12
     },
     {
         "categoryName": "Music/Audio",
         "percentApps": 0.042,
-        "percentPopApps": 0.011
+        "percentPopApps": 0.011,
+        id: 13
     },
     {
         "categoryName": "News",
         "percentApps": 0.032,
-        "percentPopApps": 0.004
+        "percentPopApps": 0.004,
+        id: 14
     },
     {
         "categoryName": "Personalized Services",
         "percentApps": 0.056,
-        "percentPopApps": 0.012
+        "percentPopApps": 0.012,
+        id: 15
     },
     {
         "categoryName": "Photography",
         "percentApps": 0.014,
-        "percentPopApps": 0.038
+        "percentPopApps": 0.038,
+        id: 16
     },
     {
         "categoryName": "Productivity",
         "percentApps": 0.025,
-        "percentPopApps": 0.016
+        "percentPopApps": 0.016,
+        id: 17
     },
     {
         "categoryName": "Shopping",
         "percentApps": 0.018,
-        "percentPopApps": 0.014
+        "percentPopApps": 0.014,
+        id: 18
     },
     {
         "categoryName": "Social",
         "percentApps": 0.022,
-        "percentPopApps": 0.012
+        "percentPopApps": 0.012,
+        id: 19
     },
     {
         "categoryName": "Sports",
         "percentApps": 0.026,
-        "percentPopApps": 0.004
+        "percentPopApps": 0.004,
+        id: 20
     },
     {
         "categoryName": "Tools",
         "percentApps": 0.064,
-        "percentPopApps": 0.016
+        "percentPopApps": 0.016,
+        id: 21
     },
     {
         "categoryName": "Transportation",
         "percentApps": 0.013,
-        "percentPopApps": 0.007
+        "percentPopApps": 0.007,
+        id: 22
     },
     {
         "categoryName": "Travel",
         "percentApps": 0.041,
-        "percentPopApps": 0.005
+        "percentPopApps": 0.005,
+        id: 23
     },
     {
         "categoryName": "Weather",
         "percentApps": 0.004,
-        "percentPopApps": 0.022
+        "percentPopApps": 0.022,
+        id: 24
     },
     {
         "categoryName": "Games",
         "percentApps": 0.188,
-        "percentPopApps": 0.032
+        "percentPopApps": 0.032,
+        id: 25
     }
 ]
 
 const appType = [{
         type: "free",
-        percentApps: 0.8511
+        percentApps: 0.8511,
+        id: 1
     },
     {
         type: "paid",
-        percentApps: 0.1489
+        percentApps: 0.1489,
+        id: 2
     }
 ]
 
@@ -220,6 +247,52 @@ const appUsage = [{
     },
 ]
 
-const appFeatures = [
+const appFeatures = [{
+        id: 1,
+        feature: "push notification",
+        description: "",
+        score: 0.00
+    },
+    {
+        id: 2,
+        feature: "geo location",
+        description: "gps",
+        score: 0.00
+    },
+    {
+        id: 3,
+        feature: "camera",
+        description: "",
+        score: 0
+    },
+    {
+        id: 4,
+        feature: "Accelerometer",
+        description: "",
+        score: 0
+    },
+    {
+        id: 5,
+        feature: "gyroscope",
+        description: "",
+        score: 0
+    },
+    {
+        id: 6,
+        feature: "Light sensor",
+        description: "",
+        score: 0
+    }
+
 
 ]
+
+const formOptions = {
+    appCategory,
+    appDiscovery,
+    appType,
+    appUsage,
+    appFeatures
+}
+
+export default formOptions
